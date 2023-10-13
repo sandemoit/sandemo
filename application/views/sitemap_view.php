52ea798fca7cdb07d144aae1b7d76e40b7c5ceb5
sandemoid <infosandemo@gmail.com>
2023-10-13 10:16:15 +0700

    fix: sitemap

--- application/views/sitemap_view.php
+++ application/views/sitemap_view.php
@@ -2,48 +2,55 @@
 header('Content-type: application/xml; charset="ISO-8859-1"', true);
 ?>
 
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <url>
-        <loc><?php echo base_url(); ?></loc>
+        <loc><?php echo base_url('/'); ?></loc>
         <priority>1.0</priority>
     </url>
+
     <!-- MAIN ROUTES -->
     <url>
-        <loc>https://sandemoindoteknologi.co.id/about</loc>
-        <lastmod>2023-06-02T03:43:40+00:00</lastmod>
+        <loc><?php echo base_url('tentang-kami'); ?></loc>
+        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://sandemoindoteknologi.co.id/service</loc>
-        <lastmod>2023-06-02T03:43:40+00:00</lastmod>
+        <loc><?php echo base_url('web-development'); ?></loc>
+        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://sandemoindoteknologi.co.id/packetappweb</loc>
-        <lastmod>2023-06-02T03:43:40+00:00</lastmod>
+        <loc><?php echo base_url('portfolio'); ?></loc>
+        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://sandemoindoteknologi.co.id/portfolio</loc>
-        <lastmod>2023-06-02T03:43:40+00:00</lastmod>
+        <loc><?php echo base_url('karir'); ?></loc>
+        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://sandemoindoteknologi.co.id/karir</loc>
-        <lastmod>2023-06-02T03:43:40+00:00</lastmod>
+        <loc><?php echo base_url('team'); ?></loc>
+        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://sandemoindoteknologi.co.id/blog</loc>
-        <lastmod>2023-06-02T03:43:40+00:00</lastmod>
+        <loc><?php echo base_url('blog'); ?></loc>
+        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
         <priority>0.80</priority>
     </url>
+    <url>
+        <loc><?php echo base_url('kontak'); ?></loc>
+        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
+        <priority>0.80</priority>
+    </url>
+
     <!-- Articles -->
     <?php foreach ($article as $data) { ?>
         <url>
             <loc><?php echo base_url('blog/') . $data->kategori . '/' . $data->slug; ?></loc>
-            <priority>1.0</priority>
             <lastmod><?php echo date('c', $data->date_created); ?></lastmod>
+            <priority>1.0</priority>
         </url>
     <?php } ?>
 </urlset>
