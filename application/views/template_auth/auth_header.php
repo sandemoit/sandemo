74f7a0bc4589d2cb53eb39bcc4e54c94ae51e696
sandemoid <infosandemo@gmail.com>
2023-12-07 11:30:11 +0700

    fix header

--- application/views/template_auth/auth_header.php
+++ application/views/template_auth/auth_header.php
@@ -17,8 +17,6 @@
     <!-- Custom CSS -->
     <link href="<?= base_url('assets') ?>/css/style.min.css" rel="stylesheet">
 
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
