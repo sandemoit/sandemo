5cc3f06dcf1c7a27420bcca20b2e53e683166b35
sandemoit <infosandemo@gmail.com>
2024-12-30 14:07:04 +0700

    fix sitemap

--- application/views/sitemap_view.php
+++ application/views/sitemap_view.php
@@ -4,53 +4,36 @@ header('Content-type: application/xml; charset="ISO-8859-1"', true);
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <url>
-        <loc><?php echo base_url('/'); ?></loc>
-        <priority>1.0</priority>
+        <loc><?= base_url('/'); ?></loc>
     </url>
 
     <!-- MAIN ROUTES -->
     <url>
-        <loc><?php echo base_url('tentang-kami'); ?></loc>
-        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
-        <priority>0.80</priority>
+        <loc><?= base_url('tentang-kami'); ?></loc>
     </url>
     <url>
-        <loc><?php echo base_url('web-development'); ?></loc>
-        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
-        <priority>0.80</priority>
+        <loc><?= base_url('web-development'); ?></loc>
     </url>
     <url>
-        <loc><?php echo base_url('portfolio'); ?></loc>
-        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
-        <priority>0.80</priority>
+        <loc><?= base_url('portfolio'); ?></loc>
     </url>
     <url>
-        <loc><?php echo base_url('karir'); ?></loc>
-        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
-        <priority>0.80</priority>
+        <loc><?= base_url('karir'); ?></loc>
     </url>
     <url>
-        <loc><?php echo base_url('team'); ?></loc>
-        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
-        <priority>0.80</priority>
+        <loc><?= base_url('team'); ?></loc>
     </url>
     <url>
-        <loc><?php echo base_url('blog'); ?></loc>
-        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
-        <priority>0.80</priority>
+        <loc><?= base_url('blog'); ?></loc>
     </url>
     <url>
-        <loc><?php echo base_url('kontak'); ?></loc>
-        <lastmod>2023-10-13T03:05:08+00:00</lastmod>
-        <priority>0.80</priority>
+        <loc><?= base_url('kontak'); ?></loc>
     </url>
 
     <!-- Articles -->
     <?php foreach ($article as $data) { ?>
         <url>
-            <loc><?php echo base_url('blog/') . $data->kategori . '/' . $data->slug; ?></loc>
-            <lastmod><?php echo date('c', $data->date_created); ?></lastmod>
-            <priority>1.0</priority>
+            <loc><?= base_url('blog/') . $data->kategori . '/' . $data->slug; ?></loc>
         </url>
     <?php } ?>
 </urlset>
